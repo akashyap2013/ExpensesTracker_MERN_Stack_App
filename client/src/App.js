7676f60df045a51284f51aef635e7f409d69b598
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-03-04 17:22:48 +0530

    Expense Header

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Expense Tracker</h1>
+  <div className="App">
+    <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+      <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
+
+      {/* grid columns */}
+      <div className="grid md:grid-cols-2 gap-4">
+          {/* Chart */}
+          {/* Form */}
+      </div>
     </div>
+  </div>
   );
 }
 
